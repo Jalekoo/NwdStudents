78972fc46439b01e2c97e9d2e045a32c82e68f24
Idriss =) <ihippocrate@gmail.com>
2013-05-15 10:21:02 +0200

    add bundles

---
+++ Nwd.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web;
+using System.Web.Optimization;
+namespace Nwd.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles( BundleCollection bundles )
+        {
+            #region Scripts
+            bundles.Add( new ScriptBundle("~/bundles/JS/bootstrap").Include("~/Scripts/bootstrap/bootstrap.min.js") );
+            bundles.Add( new ScriptBundle( "~/bundles/JS/knockout" ).Include( "~/Scripts/knockout-2.2.1.js" ) );
+            #endregion
+
+            #region CSS
+            #endregion
+        }
+    }
+}
--- Nwd.Web/Global.asax.cs
+++ Nwd.Web/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using System.Data.Entity;
 using Nwd.BackOffice.Model;
@@ -23,7 +24,7 @@ namespace Nwd.Web
             WebApiConfig.Register( GlobalConfiguration.Configuration );
             FilterConfig.RegisterGlobalFilters( GlobalFilters.Filters );
             RouteConfig.RegisterRoutes( RouteTable.Routes );
-
+            BundleConfig.RegisterBundles( BundleTable.Bundles );
             Database.SetInitializer( new DropCreateDatabaseAlways<NwdBackOfficeContext>() );
             Database.SetInitializer( new DropCreateDatabaseAlways<NwdFrontOfficeContext>() );
 
