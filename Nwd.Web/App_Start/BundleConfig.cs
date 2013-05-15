b6c14a773e1f1ff7d92942ebd663744f77fbab03
Idriss =) <ihippocrate@gmail.com>
2013-05-15 11:19:41 +0200

    fix bundle issue

--- Nwd.Web/App_Start/BundleConfig.cs
+++ Nwd.Web/App_Start/BundleConfig.cs
@@ -9,16 +9,16 @@ namespace Nwd.Web
         {
             #region Scripts
             bundles.IgnoreList.Ignore( "*.map", OptimizationMode.Always );
-            bundles.Add( new ScriptBundle( "~/bundles/JS/jquery" ).Include( "~/Scripts/jquery-{version}.js" ).Include("~/Scripts/jquery-ui-1.10.2.min.js" ));
-            bundles.Add( new ScriptBundle("~/bundles/JS/bootstrap").Include("~/Scripts/bootstrap/js/bootstrap.min.js") );
+            bundles.Add( new ScriptBundle( "~/bundles/JS/jquery" ).Include( "~/Scripts/jquery-{version}.js" ).Include("~/Scripts/jquery-ui-1.10.2.js" ));
+            bundles.Add( new ScriptBundle( "~/bundles/JS/bootstrap").Include("~/Scripts/bootstrap/js/bootstrap.js") );
             bundles.Add( new ScriptBundle( "~/bundles/JS/knockout" ).Include( "~/Scripts/knockout-2.2.1.js" ) );
             
             #endregion
 
             #region CSS
             bundles.Add( new StyleBundle( "~/bundles/CSS/Layout" ).Include("~/Content/Layout.css") );
-            bundles.Add( new StyleBundle( "~/bundles/CSS/bootstrap" ).Include( "~/Scripts/bootstrap/css/bootstrap.min.css",
-                "~/Scripts/bootstrap/css/bootstrap-responsive.min.css" ) );
+            bundles.Add( new StyleBundle( "~/bundles/CSS/bootstrap" ).Include( "~/Scripts/bootstrap/css/bootstrap.css",
+                "~/Scripts/bootstrap/css/bootstrap-responsive.css" ) );
             #endregion
         }
     }
