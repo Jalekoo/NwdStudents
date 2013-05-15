4f0e5e602a85a1bca6edea7e3f9ad8813d8632e6
Idriss =) <ihippocrate@gmail.com>
2013-05-15 10:36:13 +0200

    add scripts and css base bundles

--- Nwd.Web/App_Start/BundleConfig.cs
+++ Nwd.Web/App_Start/BundleConfig.cs
@@ -8,11 +8,14 @@ namespace Nwd.Web
         public static void RegisterBundles( BundleCollection bundles )
         {
             #region Scripts
+            bundles.IgnoreList.Ignore( "*.map", OptimizationMode.Always );
+            bundles.Add( new ScriptBundle( "~/bundles/JS/jquery" ).Include("~/Scripts/jquery-{version}.js" ) );
             bundles.Add( new ScriptBundle("~/bundles/JS/bootstrap").Include("~/Scripts/bootstrap/bootstrap.min.js") );
             bundles.Add( new ScriptBundle( "~/bundles/JS/knockout" ).Include( "~/Scripts/knockout-2.2.1.js" ) );
             #endregion
 
             #region CSS
+            bundles.Add( new StyleBundle( "~/bundles/CSS/Layout" ).Include("~/Content/Layout.less") );
             #endregion
         }
     }
