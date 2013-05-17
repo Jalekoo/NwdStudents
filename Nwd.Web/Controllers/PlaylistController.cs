17901fda47a8b1a342a9f1099e581fd5b611801b
Julien Mathon <julien.mathon@invenietis.com>
2013-05-17 10:52:43 +0200

    Change front layout

---
+++ Nwd.Web/Controllers/PlaylistController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nwd.Web.Controllers
+{
+    public class PlaylistController : Controller
+    {
+        //
+        // GET: /Playlist/
+
+        [Authorize( Roles = "User" )]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
